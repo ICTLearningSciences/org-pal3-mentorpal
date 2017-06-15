b10c08a2d10223f9cf114d731d6910680957fcf3
Kayla Carr <kcarr@ict.usc.edu>
2017-06-15 11:22:19 -0700

    Updated run2.py

--- src/classifier/run2.py
+++ src/classifier/run2.py
@@ -16,47 +16,41 @@ Use this as an example to write your own run.py file which will do the following
 ec=ensemble.EnsembleClassifier()
 
 #in pipe from Unity
-print("CREATING PIPE...")
 in_pipe = win32file.CreateFile("\\\\.\\pipe\\pipe_unity", win32file.GENERIC_READ | win32file.GENERIC_WRITE, 0, None, win32file.OPEN_EXISTING, 0, None)
-print("CREATING READER...")
 read_fd = msvcrt.open_osfhandle(in_pipe, os.O_RDONLY)
 reader = open(read_fd, "r")
-print("READING PIPE...")
 input = reader.readline()
-print("READ: " + input)
 
 #out pipe to Unity
-print("CREATING PIPE...")
 out_pipe = win32pipe.CreateNamedPipe(r'\\.\pipe\pipe_python', win32pipe.PIPE_ACCESS_DUPLEX, win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_WAIT, 1, 65536, 65536, 300, None)
-print("CONNECTING TO PIPE...")
 win32pipe.ConnectNamedPipe(out_pipe, None)
-print("CREATING WRITER...")
 write_fd = msvcrt.open_osfhandle(out_pipe, os.O_WRONLY)
 writer = open(write_fd, "w")
-print("MENTOR PAL IS READY TO GO")
 
 end_flag = False
+
 while end_flag == False:
-    print("READ INPUT: " + input)
-    if "_READY_" in input:
+    print("input = " + input)
+    input = input.replace('\r', '')
+    input = input.replace('\n', '')
+
+    if input == "_READY_":
         writer.write("_READY_")
-    elif "_QUIT_" in input:
+    elif input == "_QUIT_":
         writer.write("_QUIT_")
         end_flag = True
         break
-    elif "_START_SESSION_" in input:
-        ec.start_session()
-        writer.write("_SESSION_STARTED_")
-    elif "_END_SESSION_" in input:
+    elif input == "_END_SESSION_":
         #ec.end_session()
-        writer.write("_SESSION_ENDED_")
+        writer.write("no_video")
+        writer.write("\n")
+        writer.write("_END_")
     else:
         answer=ec.process_input_from_ui(input)
         writer.write(answer[0])
         writer.write("\n")
         writer.write(answer[1])
-        print(answer[0])
-        print(answer[1])
+
     writer.flush()
     input = reader.readline()
 
