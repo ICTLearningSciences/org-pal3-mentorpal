649bab392eaf66a5f7405ecbc78312aeb95a7717
Kayla Carr <kcarr@ict.usc.edu>
2017-06-20 10:35:50 -0700

    run2.py uses npceditor mode only

--- src/classifier/run2.py
+++ src/classifier/run2.py
@@ -4,16 +4,10 @@ import win32file
 import msvcrt
 import os
 
-'''
-This file is just a sample on how to start a session, ask questions to the program and end the session.
-Use this as an example to write your own run.py file which will do the following:
-
-1. Start a session
-2. Ask questions and receives answers
-3. Sends signals to ensemble.py to handle situations that might arise.
-4. Ends session by setting end_flag=True
-'''
-ec=ensemble.EnsembleClassifier()
+#mode='npceditor' will fetch answers only from npceditor.
+#mode='classifier' will fetch answers only from classifier.
+#mode='ensemble' will fetch answers from both classifier and ensemble and decide the best
+ec=ensemble.EnsembleClassifier(mode='npceditor')
 
 # open server pipe from Unity
 pipe = win32file.CreateFile("\\\\.\\pipe\\pipe_unity", win32pipe.PIPE_ACCESS_DUPLEX, 0, None, win32file.OPEN_EXISTING, 0, None)
@@ -23,24 +17,29 @@ input = reader.readline()
 write_fd = msvcrt.open_osfhandle(pipe, os.O_WRONLY)
 writer = open(write_fd, "w")
 
+print("Interface is ready")
 end_flag = False
-
 while end_flag == False:
+
     print("input = " + input)
     input = input.replace('\r', '')
     input = input.replace('\n', '')
 
     if input == "_READY_":
         writer.write("_READY_")
+
     elif input == "_QUIT_":
         writer.write("_QUIT_")
         end_flag = True
         break
+
     else:
-        response=ec.process_input_from_ui(input)
-        writer.write(response[0])
+        video_file, transcript = ec.process_input_from_ui(input)
+        print(video_file)
+        print(transcript)
+        writer.write(video_file)
         writer.write("\n")
-        writer.write(response[1])
+        writer.write(transcript)
 
     writer.flush()
     input = reader.readline()
