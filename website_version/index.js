3d0d2d85a42c384a7e62362a51eaf69f38e9291a
Kenneth Shaw <kshaw@ict.usc.edu>
2018-06-05 09:49:34 -0700

    Updated version for the mobile site

--- website_version/index.js
+++ website_version/index.js
@@ -1,16 +1,30 @@
+var globalResults;
 function resizeFix(){
-	if (screen.width<800){	//check if we're on mobile or not
+	if (screen.width<800){	//check if we're on mobile
 		document.getElementById("mainSize").className = "container-fluid";
+		document.getElementById("topic-box").className = "topic-box-mobile";
+		renderButtons(globalResults);
 	} else {
 		document.getElementById("mainSize").className = "container";
+		document.getElementById("topic-box").className = "topic-box";
+		renderButtons(globalResults);
 	}
 }
-resizeFix();
 
-Papa.parse("https://raw.githubusercontent.com/benjamid/MentorPAL/master/mentors/clint/data/topics.csv", {	//setup the buttons
+
+Papa.parse("https://raw.githubusercontent.com/benjamid/MentorPAL/master/mentors/clint/data/topics.csv", {	//setup the csv for buttons on desktop
 	download: true,
-	complete: function(results) {						
+	complete: function(results) {	
+		globalResults = results;
+		resizeFix();	//run this after we get the button names
+	}
+});
+
+function renderButtons(results){
+	document.getElementById("topic-box").innerHTML = '';
+	if (screen.width>=800){	//we shouldn't check this each loop so goes on the outside
 		for (var i = 0; i<results.data.length-3; i++){
+		///////////////This is the desktop version	
 			if (i%5==0){	//create rows for the buttons
 				var buttonrow = document.createElement("div");
 				buttonrow.class="row";
@@ -25,11 +39,31 @@ Papa.parse("https://raw.githubusercontent.com/benjamid/MentorPAL/master/mentors/
 			btn.value = name;
 			btn.onclick = function() {findquestion(this)};	//on click find a question
 			buttonrow.appendChild(btn);	//append button to row
-		}
-	}
-});
+		}		
+	} else{
+	//////////////////////This is the mobile version
+		for (var i = 0; i<results.data.length-3; i++){
+		///////////////This is the desktop version	
+			if (i%(results.data.length/4)==0){	//create rows for the buttons
+				var buttonrow = document.createElement("div");
+				buttonrow.class="row";
+				buttonrow.style.paddingBottom="0.5%";
+				buttonrow.style.paddingTop="0.25%";
+				document.getElementById("topic-box").appendChild(buttonrow);
+			}				
+			btn = document.createElement("BUTTON");        // Create buttons
+			btn.className = "btn button-settings-mobile";
+			var name = results.data[i][0];
+			btn.appendChild(document.createTextNode(name)); 
+			btn.value = name;
+			btn.onclick = function() {findquestion(this)};	//on click find a question
+			buttonrow.appendChild(btn);	//append button to row
+		}		
+	}	
+}
 
 var x = {};	//hold the amount of times button has already been clicked
+
 function findquestion(thisButton) {	//find the question that needs to be filled into the send box
 	//console.log(thisButton.value);
 	if (x[thisButton.value]){		//Keep track of which question in the topic list we're on
