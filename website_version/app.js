5eb65390e329979a4b66af65324639568aa4fded
Kenneth Shaw <kshaw@ict.usc.edu>
2018-06-12 12:14:46 -0700

    removed some dependencies for lighter docker

--- website_version/app.js
+++ website_version/app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var cons = require('consolidate');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,11 +13,12 @@ app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
-app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
