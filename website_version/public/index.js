34bc65a2e80a64e114523cc547799d49611c533b
Kenneth Shaw <kshaw@ict.usc.edu>
2018-06-13 15:09:00 -0700

    fixing buttons

--- website_version/public/index.js
+++ website_version/public/index.js
@@ -32,8 +32,9 @@ if (mentorID == 'clint'){
 		videoURL: "https://pal3-dev.ict.usc.edu/resources/mentor/clint/",
 		idleURL: "https://pal3-dev.ict.usc.edu/resources/mentor/clint/clintanderson_I_7_1.ogv",
 		topicsURL: "https://raw.githubusercontent.com/benjamid/MentorPAL/master/mentors/clint/data/topics.csv",
-		questions: "https://raw.githubusercontent.com/benjamid/MentorPAL/master/mentors/dan/data/Questions_Paraphrases_Answers.csv"
+		questions: "https://raw.githubusercontent.com/benjamid/MentorPAL/master/mentors/dan/data/Questions_Paraphrases_Answers.csv",
 		intro: "My name is EMC Clint Anderson, that's Electrician's Mate Clinton Anderson. I was born in Los Angeles, California. I was raised there most of my life and I graduated from high school there. A couple of years after graduating from high school, then I joined the United States Navy. I was an Electrician's Mate for eight years. I served on an aircraft carrier. We went on many deployments. A deployment is when you go to war, you fight. We fought in the Iraq war. I went on three deployments and it was a really great time in my life. I had a lot of fun. At the end of the eight years, I decided that the Navy wasn't quite a career for me. So, I got out of the Navy. I started using the education benefits that we received and I started going to the University of California at Berkeley. I was majoring in computer science and afterwards, I started getting my master's degree from the University of Southern California. I also had a job at the Institute for Creative Technologies. It's been a lot of fun, this whole time. Thanks to the Navy."
+
 	};
 }
 
@@ -91,10 +92,8 @@ function renderButtons(results){
 		}
 	} else{
 	//////////////////////This is the mobile version
-		
-		var length = (results.data.length-3-((results.data.length-3)%3))/3;
 		for (var i = 0; i<results.data.length-3; i++){			
-			if (i%(results.data.length/2)<1){	//create rows for the buttons 
+			if (i%((results.data.length-3)/2)<1){	//create rows for the buttons results.data.length/2
 				var buttonrow = document.createElement("div");
 				buttonrow.class="row";
 				buttonrow.style.paddingBottom="0.5%";
