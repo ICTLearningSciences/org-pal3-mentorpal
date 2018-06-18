be734aa2894ddb3c0074f0ffb0c359541e7e491a
Kenneth Shaw <kshaw@ict.usc.edu>
2018-06-18 09:40:54 -0700

    https

--- website_version/app.js
+++ website_version/app.js
@@ -7,12 +7,18 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var http = require('http').Server(app);
+var https = require('https');
 // view engine setup
 app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
+var options = {
+  key: fs.readFileSync('/etc/letsencrypt/keys/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/keys/fullchain.pem')
+};
+var server = https.createServer(options, app);
+var io = require('socket.io')(server);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,8 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-indexRouter.io.listen(http);
-http.listen(80, function(){
+indexRouter.io.listen(https);
+server.listen(443, function(){
     console.log('Listening for http requests');
 })
 module.exports = app;
