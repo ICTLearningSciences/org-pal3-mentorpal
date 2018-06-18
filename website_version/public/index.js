4f625162ab97615fcbf3f65810c7ff6d464622ad
Kenneth Shaw <kshaw@ict.usc.edu>
2018-06-18 13:45:59 -0700

    fixing default mentor

--- website_version/public/index.js
+++ website_version/public/index.js
@@ -30,12 +30,12 @@ if (mentorID == 'clint'){
 	mentorID = 'clint';
 	mentor = {
 		name: "Clinton Anderson",
-		videoURL: "https://pal3-dev.ict.usc.edu/resources/mentor/dan/",
-		idleURL: "https://pal3-dev.ict.usc.edu/resources/mentor/dan/idle.mp4",
-		topicsURL: "/clint/topics.csv",
-		questions: "/clint/Questions_Paraphrases_Answers.csv",
+		videoURL: "https://pal3-dev.ict.usc.edu/resources/mentor/clint/",
+		idleURL: "https://pal3-dev.ict.usc.edu/resources/mentor/clint/idle.mp4",
+		topicsURL: "topics.csv",
+		questions: "Questions_Paraphrases_Answers.csv",
 		intro: "My name is EMC Clint Anderson, that's Electrician's Mate Clinton Anderson. I was born in Los Angeles, California. I was raised there most of my life and I graduated from high school there. A couple of years after graduating from high school, then I joined the United States Navy. I was an Electrician's Mate for eight years. I served on an aircraft carrier. We went on many deployments. A deployment is when you go to war, you fight. We fought in the Iraq war. I went on three deployments and it was a really great time in my life. I had a lot of fun. At the end of the eight years, I decided that the Navy wasn't quite a career for me. So, I got out of the Navy. I started using the education benefits that we received and I started going to the University of California at Berkeley. I was majoring in computer science and afterwards, I started getting my master's degree from the University of Southern California. I also had a job at the Institute for Creative Technologies. It's been a lot of fun, this whole time. Thanks to the Navy.",
-		introURL: "https://pal3-dev.ict.usc.edu/resources/mentor/dan/dandavis_A1_1_1.mp4"
+		introURL: "https://pal3-dev.ict.usc.edu/resources/mentor/clint/clintanderson_A1_1_1.mp4"
 	};
 }
 
