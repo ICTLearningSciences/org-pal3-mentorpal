23123c1bf3171c7ebe1d4370b26a29a2e517f4c0
Ubuntu <ubuntu@ip-172-31-38-254.us-east-2.compute.internal>
2018-06-18 19:25:50 +0000

    Added https keys

--- website_version/app.js
+++ website_version/app.js
@@ -5,17 +5,18 @@ var cookieParser = require('cookie-parser');
 var cons = require('consolidate');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var fs = require('fs');
 var app = express();
 var https = require('https');
+var http = require('http');
 // view engine setup
 app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
 var options = {
-  key: fs.readFileSync('/etc/letsencrypt/keys/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/keys/fullchain.pem')
+  key: fs.readFileSync('/etc/letsencrypt/live/mentorpal.org/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/mentorpal.org/fullchain.pem')
 };
 var server = https.createServer(options, app);
 var io = require('socket.io')(server);
@@ -27,8 +28,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-indexRouter.io.listen(https);
 server.listen(443, function(){
     console.log('Listening for http requests');
-})
+});
+indexRouter.io.listen(server);
+
 module.exports = app;
+
+
