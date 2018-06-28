997eab295309a88b979a5edbdf66ccdd08e2ca1b
Kenneth Shaw <kshaw@ict.usc.edu>
2018-06-28 12:05:27 -0700

    changing up format

--- website_version/public/index.js
+++ website_version/public/index.js
@@ -28,15 +28,15 @@ if (mentorID == 'clint'){
 		introURL: "dandavis_A1_1_1"
 	};
 } else {	//if it's none of these default to clint.  You could redirect to a homepage too that's why this is here
-	mentorID = 'clint';
+	mentorID = 'julianne';
 	mentor = {
-		name: "Clinton Anderson",
-		videoURL: "https://pal3.ict.usc.edu/resources/mentor/clint/",
+		name: "Julianne Nordhagen",
+		videoURL: "https://pal3.ict.usc.edu/resources/mentor/julianne/",
 		idleURL: "https://pal3.ict.usc.edu/resources/mentor/clint/idle",
-		topicsURL: "/clint/topics.csv",
-		questions: "/clint/Questions_Paraphrases_Answers.csv",
-		intro: "My name is EMC Clint Anderson, that's Electrician's Mate Clinton Anderson. I was born in Los Angeles, California. I was raised there most of my life and I graduated from high school there. A couple of years after graduating from high school, then I joined the United States Navy. I was an Electrician's Mate for eight years. I served on an aircraft carrier. We went on many deployments. A deployment is when you go to war, you fight. We fought in the Iraq war. I went on three deployments and it was a really great time in my life. I had a lot of fun. At the end of the eight years, I decided that the Navy wasn't quite a career for me. So, I got out of the Navy. I started using the education benefits that we received and I started going to the University of California at Berkeley. I was majoring in computer science and afterwards, I started getting my master's degree from the University of Southern California. I also had a job at the Institute for Creative Technologies. It's been a lot of fun, this whole time. Thanks to the Navy.",
-		introURL: "clintanderson_A1_1_1"
+		topicsURL: "/julianne/topics.csv",
+		questions: "/julianne/Questions_Paraphrases_Answers.csv",
+		intro: "Hi my name's Julie in Oregon I'm %HESITATION in and sit in the United States Navy and I'm currently a student naval aviator so that means that I have commissioned into the navy and I am starting to learn how to fly planes and %HESITATION will then become a full trained pilot for the navy",
+		introURL: "julianne_U1_1_1"
 	};
 }
 var isMobile="";
