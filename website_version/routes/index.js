d64c2ef75c56b8b16a9c8dcd3d759b0e0303baaf
Kenneth Shaw <kshaw@ict.usc.edu>
2018-06-28 13:53:11 -0700

    updating routes

--- website_version/routes/index.js
+++ website_version/routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var http = require('https');
+var http = require('http');
 var router = express.Router();
 var io = require('socket.io')(http);
 var watson = require('watson-developer-cloud');
@@ -8,7 +8,8 @@ var fs = require('fs');
 //https://github.com/extrabacon/python-shell/issues/113 : /python-shell/index.js, on line 72
 //After installation, comment out the warning printing if tensorflow is having trouble importing
 var options = {
-    pythonPath: '/usr/bin/python3',
+    //pythonPath: 'E:/Python35/python.exe',
+    pythonPath: '/usr/bin/python3'
 }
 var pyshell = new PythonShell('/../src/classifier/website_interface.py',options);
 var text = fs.readFileSync('./password.txt','utf8');
@@ -22,7 +23,7 @@ var authorization = new watson.AuthorizationV1({
 var params = {
     url: 'https://gateway.watsonplatform.net/speech-to-text/api'
 };
-
+//********add a mentor here: copy paste the pattern*/
 router.get('/', function(req, res, next) {
   res.render('index');
 });
@@ -38,6 +39,13 @@ router.get('/dan', function(req, res, next) {
 router.get('/dan/embed', function(req, res, next){
   res.render('index');
 });
+router.get('/julianne', function(req, res, next) {
+  res.render('index');
+});
+router.get('/julianne/embed', function(req, res, next){
+  res.render('index');
+});
+//***********add above*/
 io.on('connection', function(socket){
     console.log('A user has socket connected');
     authorization.getToken(params, function (err, token) {  //get watson token
@@ -51,6 +59,7 @@ io.on('connection', function(socket){
         console.log(data);
         /*do the question processing stuff here*/
         pyshell.send(data.Question+','+socket.id+','+data.Mentor);
+        console.log(data.Question+','+socket.id+','+data.Mentor);
     });
 });
 
@@ -58,8 +67,9 @@ pyshell.on('message',function(message){ //if a message is recieved from python
     message = message.split('(');
     if (message[1]){    //this is the message we need
         message = message[1];
-        processedOutput = {"videoID": message.slice(1,message.indexOf(",")-1),"transcript": message.slice(message.indexOf(",")+3,message.indexOf(")")-1),"clientID": message.slice(message.indexOf(")")+1)}
-        io.to(processedOutput.clientID).emit("receiveAnswer",{"videoID": processedOutput.videoID, "transcript": processedOutput.transcript});    //sends back to the same client
+        processedOutput = {"videoID": message.slice(1,message.indexOf(",")-1),"transcript": message.slice(message.indexOf(",")+3,message.indexOf(")")-1),"clientID": message.slice(message.indexOf(")")+1).replace("\r","")}
+        console.log(processedOutput);
+		io.to(processedOutput.clientID).emit("receiveAnswer",{"videoID": processedOutput.videoID, "transcript": processedOutput.transcript});    //sends back to the same client
         console.log('videoID: ' + processedOutput.videoID);
         console.log('transcript: '+ processedOutput.transcript);
     }
