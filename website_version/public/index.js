d6a053f37e9e05e02c4ca392b6a390d28e17043f
Kenneth Shaw <kshaw@ict.usc.edu>
2018-06-29 14:27:47 -0700

    New Title area for mentors

--- website_version/public/index.js
+++ website_version/public/index.js
@@ -15,7 +15,8 @@ if (mentorID == 'clint'){
 		topicsURL: "/clint/topics.csv",
 		questions: "/clint/Questions_Paraphrases_Answers.csv",
 		intro: "My name is EMC Clint Anderson, that's Electrician's Mate Clinton Anderson. I was born in Los Angeles, California. I was raised there most of my life and I graduated from high school there. A couple of years after graduating from high school, then I joined the United States Navy. I was an Electrician's Mate for eight years. I served on an aircraft carrier. We went on many deployments. A deployment is when you go to war, you fight. We fought in the Iraq war. I went on three deployments and it was a really great time in my life. I had a lot of fun. At the end of the eight years, I decided that the Navy wasn't quite a career for me. So, I got out of the Navy. I started using the education benefits that we received and I started going to the University of California at Berkeley. I was majoring in computer science and afterwards, I started getting my master's degree from the University of Southern California. I also had a job at the Institute for Creative Technologies. It's been a lot of fun, this whole time. Thanks to the Navy.",
-		introURL: "clintanderson_A1_1_1"
+		introURL: "clintanderson_A1_1_1",
+		title: "Clinton Anderson: Nuclear Electrician's Mate"
 	};
 } else if (mentorID == 'dan'){
 	mentor = {
@@ -25,7 +26,8 @@ if (mentorID == 'clint'){
 		topicsURL: "/dan/topics.csv",
 		questions: "/dan/Questions_Paraphrases_Answers.csv",
 		intro: "Hello I'm Dan Davis I've worked for universities to last thirty years doing basic research in high performance computing of work for Cal Tech, University of Southern California and the University of Hawaii",
-		introURL: "dandavis_A1_1_1"
+		introURL: "dandavis_A1_1_1",
+		title: "Dan Davis: High Performance Computing Researcher"
 	};
 } else {	//if it's none of these default to clint.  You could redirect to a homepage too that's why this is here
 	mentorID = 'julianne';
@@ -36,7 +38,8 @@ if (mentorID == 'clint'){
 		topicsURL: "/julianne/topics.csv",
 		questions: "/julianne/Questions_Paraphrases_Answers.csv",
 		intro: "Hi my name's Julie in Oregon I'm %HESITATION in and sit in the United States Navy and I'm currently a student naval aviator so that means that I have commissioned into the navy and I am starting to learn how to fly planes and %HESITATION will then become a full trained pilot for the navy",
-		introURL: "julianne_U1_1_1"
+		introURL: "julianne_U1_1_1",
+		title: "Julianne Nordhagen: Student Naval Aviator"
 	};
 }
 var isMobile="";
@@ -234,7 +237,7 @@ video.onended = function(){		//when the video playing finishes, play the idle vi
 	video.controls = false;
 }
 document.getElementById("caption-box").innerHTML = '' + '<b> Mentor: </b>' + '\xa0\xa0'  +  mentor.intro +'<br>';
-
+document.getElementById("mentor-title").textContent = mentor.title;
 $('#question-Box').keydown(function(e) {
     if (e.keyCode === 13) {
         $(this).val('').focus();
