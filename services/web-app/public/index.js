e8d498ada88a1a08bf6976b4cf4da697290a256e
Larry Kirschner <larrykirschner@gmail.com>
2019-04-02 12:56:41 -0700

    updates app to replace pal.ict.usc.edu with video.mentorpal.org as base of video url
    NOTE: this is structured poorly still.
    The urls should not be hard coded into the client code.
    In a future rev, we can fix this, but maybe defer until
    we see if we're going to rebuild the client in a tighter
    and more modern framework, e.g. React

--- services/web-app/public/index.js
+++ services/web-app/public/index.js
@@ -1,74 +1,66 @@
 var globalResults
 var socket = io()
 var video = document.getElementById("videoPlayer")
-var mobileSuffix=""
+var videoTargetType="web"
 urlp=[];u=location.search.replace("?","").split("&").forEach(function(d){e=d.split("=");urlp[e[0]]=e[1];})
 const isUnity=urlp["unity"]
 const mentorID = window.location.pathname.split("/")[1]
 const username = localStorage.getItem("username")
-var mentor = {}
 var blacklist = []
 const num_blacklisted_repeats = 5
 document.getElementById("user-display").textContent = username
 
-//Each mentor needs its own set of links
-//This way, content can be hosted elsewhere explicit
-if (mentorID == 'clint') {
-	mentor = {
-		name: "Clint Anderson",
-		shortName: "Clint", //for the transcript
-		intro: "My name is EMC Clint Anderson, that's Electrician's Mate Clinton Anderson. I was born in Los Angeles, California. I was raised there most of my life and I graduated from high school there. A couple of years after graduating from high school, then I joined the United States Navy. I was an Electrician's Mate for eight years. I served on an aircraft carrier. We went on many deployments. A deployment is when you go to war, you fight. We fought in the Iraq war. I went on three deployments and it was a really great time in my life. I had a lot of fun. At the end of the eight years, I decided that the Navy wasn't quite a career for me. So, I got out of the Navy. I started using the education benefits that we received and I started going to the University of California at Berkeley. I was majoring in computer science and afterwards, I started getting my master's degree from the University of Southern California. I also had a job at the Institute for Creative Technologies. It's been a lot of fun, this whole time. Thanks to the Navy.",
-		introVideoId: "clintanderson_A1_1_1",
-		title: "Clinton Anderson: Nuclear Electrician's Mate" //for the title
-	};
-} else if (mentorID == 'dan') {
-	mentor = {
-		name: "Dan Davis",
-		shortName: "Dan",
-		intro: "Hello I'm Dan Davis I've worked for universities to last thirty years doing basic research in high performance computing of work for Cal Tech, University of Southern California and the University of Hawaii",
-		introVideoId: "dandavis_A1_1_1",
-		title: "Dan Davis: High Performance Computing Researcher"
-	};
-} else if (mentorID == 'julianne') {	//if it's none of these default to clint.  You could redirect to a homepage too that's why this is here
-	mentor = {
-		name: "Julianne Nordhagen",
-		shortName: "Julianne",
-		intro: "Hi my name's Julie Nordhagen, I'm in the United States Navy and I'm currently a student naval aviator so that means that I have commissioned into the Navy and I am starting to learn how to fly planes and will then become a full trained pilot for the Navy.",
-		introVideoId: "julianne_U1_1_1",
-		title: "Julianne Nordhagen: Student Naval Aviator"
-	};
-} else if (mentorID == 'carlos') {	//if it's none of these default to clint.  You could redirect to a homepage too that's why this is here
-	mentor = {
+const MENTOR_API_URL = '/mentor-api'
+const MENTOR_VIDEO_HOST = 'https://video.mentorpal.org' // TODO: pass from server/env variable
+const createMentor = function(mId, data) {
+	const videoURLFor = function(id) {
+		return `${MENTOR_VIDEO_HOST}/videos/mentors/${mId}/${videoTargetType}/${id}.mp4`
+	}
+	return { 
+		...data,
+		topicsURL: `${MENTOR_API_URL}/mentors/${mId}/data/topics.csv`,
+		questions: `${MENTOR_API_URL}/mentors/${mId}/data/Questions_Paraphrases_Answers.csv`,
+		classifier: `${MENTOR_API_URL}/mentors/${mId}/data/classifier_data.csv`,
+		idleURL: videoURLFor('idle'),
+		trackUrlFor: function(id) {
+			return `${MENTOR_API_URL}/mentors/${mId}/tracks/${id}.vtt`
+		},
+		videoURLFor: videoURLFor
+	}
+}
+
+const mentorDataById = {
+	carlos: {
 		name: "Carlos Rios",
 		shortName: "Carlos",
 		intro: "So my name is Carlos Rios. I'm a logistics lead supporting marine corps projects. I'm originally from Connecticut or New Haven, Connecticut. My mother and father are from Puerto Rico they migrated over to Connecticut and then from there after about six well I was about seven years old and moved over to a Philadelphia where I spent most of my most of my youth. About age 18-19 years old graduated high school and joined the marine corps. Twenty three years later, retired. During that time of course I got married. I have been married for twenty seven years. I have two great kids, one currently attending USC and one in the near future want to attend Clemson, South Carolina where I currently reside after my retirement from the marine corps. I spent two years as a contractor supporting the marine corps and I personally think I did such a good job that the government decided to bring it over to that side and support as a government employee and I've been doing that for about seven years high manage everything from my computer, servers, laptops to drones.",
 		introVideoId: "carlos_A1_1_1",
 		title: "Carlos Rios: Marine Logistician"
-	};
-} else {
-	mentorID = 'clint';
-	mentor = {
-		name: "Clinton Anderson",
+	},
+	clint: {
+		name: "Clint Anderson",
+		shortName: "Clint", //for the transcript
 		intro: "My name is EMC Clint Anderson, that's Electrician's Mate Clinton Anderson. I was born in Los Angeles, California. I was raised there most of my life and I graduated from high school there. A couple of years after graduating from high school, then I joined the United States Navy. I was an Electrician's Mate for eight years. I served on an aircraft carrier. We went on many deployments. A deployment is when you go to war, you fight. We fought in the Iraq war. I went on three deployments and it was a really great time in my life. I had a lot of fun. At the end of the eight years, I decided that the Navy wasn't quite a career for me. So, I got out of the Navy. I started using the education benefits that we received and I started going to the University of California at Berkeley. I was majoring in computer science and afterwards, I started getting my master's degree from the University of Southern California. I also had a job at the Institute for Creative Technologies. It's been a lot of fun, this whole time. Thanks to the Navy.",
 		introVideoId: "clintanderson_A1_1_1",
-		title: "Clinton Anderson: Nuclear Electrician's Mate"
-	};
+		title: "Clinton Anderson: Nuclear Electrician's Mate" //for the title
+	},
+	dan: {
+		name: "Dan Davis",
+		shortName: "Dan",
+		intro: "Hello I'm Dan Davis I've worked for universities to last thirty years doing basic research in high performance computing of work for Cal Tech, University of Southern California and the University of Hawaii",
+		introVideoId: "dandavis_A1_1_1",
+		title: "Dan Davis: High Performance Computing Researcher"
+	},
+	julianne: {
+		name: "Julianne Nordhagen",
+		shortName: "Julianne",
+		intro: "Hi my name's Julie Nordhagen, I'm in the United States Navy and I'm currently a student naval aviator so that means that I have commissioned into the Navy and I am starting to learn how to fly planes and will then become a full trained pilot for the Navy.",
+		introVideoId: "julianne_U1_1_1",
+		title: "Julianne Nordhagen: Student Naval Aviator"
+	}
 }
 
-
-const MENTOR_API_URL = '/mentor-api'
-const MENTOR_VIDEO_HOST = 'https://video.mentorpal.org' // TODO: pass from server/env variable
-
-mentor.topicsURL = `${MENTOR_API_URL}/mentors/${mentorID}/data/topics.csv`
-mentor.questions = `${MENTOR_API_URL}/mentors/${mentorID}/data/Questions_Paraphrases_Answers.csv`
-mentor.classifier = `${MENTOR_API_URL}/mentors/${mentorID}/data/classifier_data.csv`
-mentor.trackUrlFor = function(id) {
-	return `${MENTOR_API_URL}/mentors/${mentorID}/tracks/${id}.vtt`
-}
-mentor.videoURLFor = function(id) {
-	return `${MENTOR_VIDEO_HOST}/videos/mentors/${mentorID}/${id}${mobileSuffix}.mp4`
-}
-mentor.idleURL = mentor.videoURLFor('idle')
+const mentor = createMentor(mentorID, mentorDataById[mentorID] || mentorDataById['clint'])
 
 //run everytime the window is resized to keep it responsive
 function resizeFix() {
@@ -76,7 +68,7 @@ function resizeFix() {
 
 	// if mobile, render this:
 	if (screen.width < 768 || isUnity == "true") {
-		mobileSuffix = "_M";
+		videoTargetType = "mobile";
 		document.getElementById("main-holder").className = "container-fluid";	// make video and button area fill screen
 		document.getElementById("videoPlayer").textTracks[0].mode = "showing";	// show subtitles
 		document.getElementById("myOverlay").innerHTML = ''
@@ -86,7 +78,7 @@ function resizeFix() {
 	}
 	// if desktop, render this
 	else {
-		mobileSuffix="";
+		videoTargetType="web";
 		document.getElementById("main-holder").className = "container";
 		document.getElementById("videoWrapper").className = 'embed-responsive embed-responsive-16by9';
 		document.getElementById("videoPlayer").textTracks[0].mode = "hidden";	// hide on-video captions
@@ -301,7 +293,7 @@ socket.on("token", function(data){
 });
 
 video.onended = function(){		//when the video playing finishes, play the idle video
-	video.src = mentor.idleURL + mobileSuffix + ".mp4";
+	video.src = mentor.idleURL;
 	document.getElementById("track").src = "";
 	video.play();
 	video.controls = false;
