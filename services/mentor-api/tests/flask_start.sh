0ff6cfa24a5c2efd7a4a3de38d2318e393f29c40
Larry Kirschner <larrykirschner@gmail.com>
2019-06-19 14:49:03 -0700

    rework CI to run build/tests for mentor-api use bash as shell for makefile cleanup submodule init update mentor-api to use same docker tag as build pass project name to env for test for mentor-api testing, pull docker image name from docker_services script get rid of docker host mounts in test

--- services/mentor-api/tests/flask_start.sh
+++ services/mentor-api/tests/flask_start.sh
@@ -6,7 +6,7 @@ if [ -z ${PROJECT_ROOT} ]; then
 	PROJECT_ROOT=$(git rev-parse --show-toplevel 2> /dev/null)
 fi
 
-IMAGE_NAME=mentorpal-mentor-api
+DOCKER_IMAGE=${DOCKER_IMAGE:-mentorpal-mentor-api}
 CONTAINER_NAME=mentorpal-mentor-api-testing
 
 # The flask docker image/api we're running
@@ -16,13 +16,13 @@ DOCKER_MOUNT_SRC=${TESTS}/docker_mount
 DOCKER_MOUNT_TGT=/app/docker_mount
 FLASK_CONFIG_TGT=${DOCKER_MOUNT_TGT}/flask_config.py
 
+echo "testing mentor-api image ${DOCKER_IMAGE}..."
+
 docker run \
 		-d \
 		--rm \
 		--name ${CONTAINER_NAME} \
 		-p 5000:5000 \
-		-v ${MENTOR_API}/src/mentor_api:/app/mentor_api \
-		-v ${PROJECT_ROOT}/classifier/src/mentorpal:/app/mentorpal \
 		-v ${DOCKER_MOUNT_SRC}:${DOCKER_MOUNT_TGT} \
 		-e MENTORPAL_CLASSIFIER_API_SETTINGS=${FLASK_CONFIG_TGT} \
-	${IMAGE_NAME}
+	${DOCKER_IMAGE}
