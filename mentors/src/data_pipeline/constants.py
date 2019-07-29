d6ff4a0b852c3c6ec7cc011f3c80503f125844d5
Mark Christenson <mark.evan.christenson@gmail.com>
2019-07-29 16:33:50 -0700

    simplify data path and constants

--- mentors/src/data_pipeline/constants.py
+++ mentors/src/data_pipeline/constants.py
@@ -1,18 +1,18 @@
 import os
 
-DATA_DIR = "data"
-RECORDINGS_DIR = os.path.join(DATA_DIR, "recordings")
-OUTPUT_DIR = "out"
-AUDIOCHUNK_DIR = os.path.join(OUTPUT_DIR, "audiochunks")
-MENTOR_DIR = os.path.join(RECORDINGS_DIR, "{}")
-SESSION_DIR = os.path.join(MENTOR_DIR, "{}")
+SHARED_DATA = "data"
+MENTOR_DATA = "{}/data"
+SESSION_DATA = "recordings/session{}"
+SESSION_OUTPUT = "out"
+AUDIOCHUNKS = "audiochunks"
 
-FILENAME = "part{}_{}"
+DATA_FILENAME = "part{}_{}"
 VIDEO_FILE = "video.mp4"
 AUDIO_FILE = "audio.wav"
 TIMESTAMP_FILE = "timestamps.csv"
-TRANSCRIPT_FILE = os.path.join(OUTPUT_DIR, "transcript.csv")
-TOPIC_MAP_FILE = os.path.join(DATA_DIR, "master_topic_map.csv")
-PARAPHRASE_MAP_FILE = os.path.join(DATA_DIR, "master_paraphrase_map.csv")
-QPA_FILE = os.path.join(DATA_DIR, "questions_paraphrases_answers.csv")
-PU_FILE = os.path.join(DATA_DIR, "prompts_utterances.csv")
+TOPIC_MAP = "master_topic_map.csv"
+PARAPHRASE_MAP = "master_paraphrase_map.csv"
+
+PU_FILENAME = "prompts_utterances.csv"
+QPA_FILENAME = "questions_paraphrases_answers.csv"
+TRANSCRIPT_FILE = "transcript.csv"
