424327ec1ca4dc04dd2309694a63f6014d89795b
Larry Kirschner <larrykirschner@gmail.com>
2019-07-30 17:33:55 -0700

    example gets one activity

--- reports/scripts/report.js
+++ reports/scripts/report.js
@@ -7,7 +7,7 @@ queryStatements({
   // verb: new TinCan.Verb({
   //   id: 'https://mentorpal.org/xapi/verb/asked',
   // }),
-  activity: 'https://dev.pal3.org/xapi/resources/5bfef7dbbecb4e208d3c856',
+  activity: 'https://dev.pal3.org/xapi/resources/5bfef7dbbecb4e208d3c856d',
   since: '2019-07-29T00:00:00Z',
 })
   .then(statements => {
