43ecb12e9ed601abe188386be713710a992ef677
larry kirschner <larrykirschner@gmail.com>
2019-09-10 23:06:19 -0700

    mentor-api uses env CHECKPOINT if CLASSIFIER_CHECKPOINT not set

--- services/mentor-api/src/mentor_api/config_default.py
+++ services/mentor-api/src/mentor_api/config_default.py
@@ -6,7 +6,7 @@ class Config(object):
         os.environ.get("SECRET_KEY") or "production_servers_must_provide_a_secret_key"
     )
     CLASSIFIER_ARCH = os.environ.get("CLASSIFIER_ARCH") or "lstm_v1"
-    CLASSIFIER_CHECKPOINT = os.environ.get("CLASSIFIER_CHECKPOINT") or "2019-06-13-1900"
+    CLASSIFIER_CHECKPOINT = os.environ.get("CLASSIFIER_CHECKPOINT") or os.environ.get("CHECKPOINT") or "2019-06-13-1900"
     CLASSIFIER_CHECKPOINT_ROOT = (
         os.environ.get("CLASSIFIER_CHECKPOINT_ROOT") or "/app/checkpoint"
     )
