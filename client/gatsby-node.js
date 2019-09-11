17edc406800c4865aefcc06c3757ef6bec1ad1ed
larry kirschner <larrykirschner@gmail.com>
2019-09-11 16:45:04 -0700

    fixes format in gatsby_node.js

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -4,7 +4,7 @@ exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "src")
+        "@": path.resolve(__dirname, "src"),
       },
       modules: [path.resolve(__dirname, "src"), "node_modules"],
     },
