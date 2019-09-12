63bbca2cdaadcfd794d80381568e4b70c24da256
larry kirschner <larrykirschner@gmail.com>
2019-09-12 07:40:36 -0700

    enables abs imports like 'import x from @/path/without/src/prefix' (#124)
    * enables abs imports like 'import x from @/path/without/src/prefix'
    
    * support @/ type paths at webpack/build time
    
    * fixes format in gatsby_node.js

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -1,15 +1,11 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
 const path = require("path");
 
 exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
       modules: [path.resolve(__dirname, "src"), "node_modules"],
     },
   });
