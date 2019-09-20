ed10260a8da92ff6b3877c2114b423808473983a
larry kirschner <larrykirschner@gmail.com>
2019-09-20 15:00:10 -0700

    remove dependency on audit-failing csv package in (deprecated) web-app

--- services/web-app/routes/validate.js
+++ services/web-app/routes/validate.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs-extra");
-const parse = require("csv-parse/lib/sync");
+// const parse = require("csv-parse/lib/sync");
 const router = express.Router();
 // const request = require('async-request');
 
