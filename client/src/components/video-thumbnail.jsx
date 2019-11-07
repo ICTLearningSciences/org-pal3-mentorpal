19a8fff505660b1726ac5e143aff699e3596d8bc
larry kirschner <larrykirschner@gmail.com>
2019-11-06 17:19:15 -0800

    client prefers configured idle url before fallback on default

--- client/src/components/video-thumbnail.jsx
+++ client/src/components/video-thumbnail.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import ReactPlayer from "react-player";
 
-import { idleUrl } from "api/api";
+import { idleUrl, videoUrl } from "api/api";
 import { MentorQuestionStatus } from "store/types";
+
+function findMentorIdleId(mentor) {
+  try {
+    return mentor.utterances_by_type["_IDLE_"][0][0];
+  } catch (err) {
+    return undefined;
+  }
+}
 
 const VideoThumbnail = ({ mentor, isMobile, width, height }) => {
   const [isPlaying, setPlaying] = useState(true);
-  const src = idleUrl(mentor.id, isMobile ? "mobile" : "web");
+  const format = isMobile ? "mobile" : "web";
   const isDisabled =
     mentor.is_off_topic || mentor.status === MentorQuestionStatus.ERROR;
 
@@ -14,10 +22,15 @@ const VideoThumbnail = ({ mentor, isMobile, width, height }) => {
     setPlaying(false);
   };
 
+  const idleVideoId = findMentorIdleId(mentor);
+  const url = idleVideoId
+    ? videoUrl(mentor.id, idleVideoId, format)
+    : idleUrl(mentor.id, format);
+
   return (
     <ReactPlayer
       style={{ opacity: isDisabled ? "0.25" : "1", backgroundColor: "black" }}
-      url={src}
+      url={url}
       height={height}
       width={width}
       onStart={onStart}
