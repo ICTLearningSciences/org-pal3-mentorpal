f466c78014c477530726cfbe133f4de5afcc86ca
larry kirschner <larrykirschner@gmail.com>
2019-11-15 18:18:50 -0800

    WIP: update all checkpoint scripts to work w no local classifier image

--- checkpoint/bin/checkpoint_test.sh
+++ checkpoint/bin/checkpoint_test.sh
@@ -1,25 +1,40 @@
-MENTOR=$1
-ARCH=$2
-CHECKPOINT=$3
-TEST_SET=testing_data_full.csv
+#!/usr/bin/env bash
+BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+${BIN}/run_classifier.sh \
+    --entrypoint /app/checkpoint/bin/checkpoint_test.py \
+    --additional-args "-e TEST_SET=testing_data_full.csv" \
+    "$@"
 
-PROJECT_ROOT=$(git rev-parse --show-toplevel 2> /dev/null)
-CHECKPOINT_ROOT=${PROJECT_ROOT}/checkpoint
 
-DOCKER_IMAGE=mentorpal-classifier
-DOCKER_CONTAINER=mentorpal-classifier
+# #!/usr/bin/env bash
+# BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# ${BIN}/run_classifier.sh \
+#     --entrypoint /app/checkpoint/bin/checkpoint_test.py \
+#     --test-set testing_data_full.csv \
+#     "$@"
 
-docker run \
-        -it \
-        --rm \
-        --name ${DOCKER_CONTAINER} \
-        -v ${CHECKPOINT_ROOT}:/app/checkpoint \
-        -v ${PROJECT_ROOT}/mentors/data/mentors:/app/mentors \
-        -v ${PROJECT_ROOT}/classifier/src/mentorpal:/app/mentorpal \
-        -e ARCH=${ARCH} \
-        -e CHECKPOINT=${CHECKPOINT} \
-        -e MENTOR=${MENTOR} \
-        -e TEST_SET=${TEST_SET} \
-        --workdir /app \
-        --entrypoint /app/checkpoint/bin/checkpoint_test.py \
-    ${DOCKER_IMAGE}
+# MENTOR=$1
+# ARCH=$2
+# CHECKPOINT=$3
+# TEST_SET=testing_data_full.csv
+
+# PROJECT_ROOT=$(git rev-parse --show-toplevel 2> /dev/null)
+# CHECKPOINT_ROOT=${PROJECT_ROOT}/checkpoint
+
+# DOCKER_IMAGE=mentorpal-classifier
+# DOCKER_CONTAINER=mentorpal-classifier
+
+# docker run \
+#         -it \
+#         --rm \
+#         --name ${DOCKER_CONTAINER} \
+#         -v ${CHECKPOINT_ROOT}:/app/checkpoint \
+#         -v ${PROJECT_ROOT}/mentors/data/mentors:/app/mentors \
+#         -v ${PROJECT_ROOT}/classifier/src/mentorpal:/app/mentorpal \
+#         -e ARCH=${ARCH} \
+#         -e CHECKPOINT=${CHECKPOINT} \
+#         -e MENTOR=${MENTOR} \
+#         -e TEST_SET=${TEST_SET} \
+#         --workdir /app \
+#         --entrypoint /app/checkpoint/bin/checkpoint_test.py \
+#     ${DOCKER_IMAGE}
--- checkpoint/bin/checkpoint_train.sh
+++ checkpoint/bin/checkpoint_train.sh
@@ -1,25 +1,5 @@
-ARCH=$1
-CHECKPOINT=$2
-TEST_MENTOR=$3
-
-PROJECT_ROOT=$(git rev-parse --show-toplevel 2> /dev/null)
-CHECKPOINT_ROOT=${PROJECT_ROOT}/checkpoint
-MENTOR_ROOT=${PROJECT_ROOT}/mentor
-
-DOCKER_IMAGE=mentorpal-classifier
-DOCKER_CONTAINER=mentorpal-classifier
-
-docker run \
-        -it \
-        --rm \
-        --name ${DOCKER_CONTAINER} \
-        -v ${CHECKPOINT_ROOT}:/app/checkpoint \
-        -v ${PROJECT_ROOT}/classifier/src/mentorpal:/app/mentorpal \
-        -v ${PROJECT_ROOT}/mentors/data/mentors:/app/mentors \
-        -e ARCH=${ARCH} \
-        -e CHECKPOINT=${CHECKPOINT} \
-        -e MENTOR_ROOT=${MENTOR_ROOT} \
-        -e TEST_MENTOR=${TEST_MENTOR} \
-        --workdir /app \
-        --entrypoint /app/checkpoint/bin/checkpoint_train.py \
-    ${DOCKER_IMAGE}
+#!/usr/bin/env bash
+BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+${BIN}/run_classifier.sh \
+    --entrypoint /app/checkpoint/bin/checkpoint_train.py \
+    "$@"
