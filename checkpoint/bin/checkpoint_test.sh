5d943517a4a3f5a2e46611f4db0e9f449c35d2d8
larry kirschner <larrykirschner@gmail.com>
2019-11-19 15:38:18 -0800

    use checkpoint bin scripts from imported module/docker image

--- checkpoint/bin/checkpoint_test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-${BIN}/run_classifier.sh \
-    --entrypoint /app/checkpoint/bin/checkpoint_test.py \
-    --additional-args "-e TEST_SET=testing_data_full.csv -v /Users/kirschner/projects/mentor-classifier/mentorpal:/opt/conda/envs/mentorpal/lib/python3.6/site-packages/mentorpal" \
-    "$@"
--- checkpoint/bin/checkpoint_train.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-${BIN}/run_classifier.sh \
-    --entrypoint /app/checkpoint/bin/checkpoint_train.py \
-    "$@"
